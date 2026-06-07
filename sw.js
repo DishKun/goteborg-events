c854e3180b68a1cebc6f3144756b2ec332d7fa9c
Kun <dishkun2012@gmail.com>
2026-06-07 23:38:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-const CACHE = 'goteborg-v2';
+const CACHE = 'goteborg-v6';
 const URLS = [
-  './',
   './index.html',
   './manifest.json',
   './icon-192.png',
@@ -9,6 +8,15 @@ const URLS = [
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+    )).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
